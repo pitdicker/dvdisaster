e2291c49040acc275d87242c600a5472692c9acb
Paul Dicker <paul@pitdicker.nl>
2025-04-03 21:27:17 +0200

    Create cairo context in expose callback

--- src/dvdisaster.h
+++ src/dvdisaster.h
@@ -692,8 +692,8 @@ int  GuiCurveX(Curve*, gdouble);
 int  GuiCurveY(Curve*, gdouble);
 int  GuiCurveLogY(Curve*, gdouble);
 
-void GuiRedrawAxes(Curve*);
-void GuiRedrawCurve(Curve*, int);
+void GuiRedrawAxes(cairo_t *cr, Curve*);
+void GuiRedrawCurve(cairo_t *cr, Curve*, int);
 #endif
 
 /***
@@ -1542,8 +1542,8 @@ void GuiSetSpiralWidget(Spiral*, GtkWidget*);
 void GuiFreeSpiral(Spiral*);
 
 void GuiFillSpiral(Spiral*, GdkColor*);
-void GuiDrawSpiral(Spiral*);
-void GuiDrawSpiralLabel(Spiral*, PangoLayout*, char*, GdkColor*, int, int);
+void GuiDrawSpiral(cairo_t *cr, Spiral*);
+void GuiDrawSpiralLabel(cairo_t *cr, Spiral*, PangoLayout*, char*, GdkColor*, int, int);
 void GuiChangeSpiralCursor(Spiral*, int);
 void GuiMoveSpiralCursor(Spiral*, int);
 void GuiSetSpiralSegmentColor(Spiral*, GdkColor*, GdkColor*, int);
--- src/rs02-verify.c
+++ src/rs02-verify.c
@@ -114,19 +114,19 @@ static void add_verify_values(Method *method, int percent,
  * Redraw whole spiral
  */
 
-static void redraw_spiral(RS02Widgets *wl)
+static void redraw_spiral(cairo_t *cr, RS02Widgets *wl)
 {  int x = wl->cmpSpiral->mx - wl->cmpSpiral->diameter/2 + 10;
 
-   GuiDrawSpiralLabel(wl->cmpSpiral, wl->cmpLayout,
+   GuiDrawSpiralLabel(cr, wl->cmpSpiral, wl->cmpLayout,
 		      _("Good sectors"), Closure->greenSector, x, 1);
 
-   GuiDrawSpiralLabel(wl->cmpSpiral, wl->cmpLayout,
+   GuiDrawSpiralLabel(cr, wl->cmpSpiral, wl->cmpLayout,
 		      _("Sectors with CRC errors"), Closure->yellowSector, x, 2);
 
-   GuiDrawSpiralLabel(wl->cmpSpiral, wl->cmpLayout,
+   GuiDrawSpiralLabel(cr, wl->cmpSpiral, wl->cmpLayout,
 		      _("Missing sectors"), Closure->redSector, x, 3);
 
-   GuiDrawSpiral(wl->cmpSpiral);
+   GuiDrawSpiral(cr, wl->cmpSpiral);
 }
 
 /*
@@ -134,7 +134,8 @@ static void redraw_spiral(RS02Widgets *wl)
  */
 
 static gboolean expose_cb(GtkWidget *widget, GdkEventExpose *event, gpointer data)
-{  RS02Widgets *wl = (RS02Widgets*)data;
+{  cairo_t *cr = gdk_cairo_create(GDK_DRAWABLE(gtk_widget_get_window(widget)));
+   RS02Widgets *wl = (RS02Widgets*)data;
    GtkAllocation a = {0};
    gtk_widget_get_allocation(widget, &a);
    int w,h,size;
@@ -153,7 +154,7 @@ static gboolean expose_cb(GtkWidget *widget, GdkEventExpose *event, gpointer dat
    wl->cmpSpiral->my = (wl->cmpSpiral->diameter + a.height - size)/2;
 
    if(!event->count)      /* Exposure compression */
-     redraw_spiral(wl);   /* Redraw the spiral */
+     redraw_spiral(cr, wl);   /* Redraw the spiral */
 
    return TRUE;
 }
