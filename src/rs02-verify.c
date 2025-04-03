68cb98720fe2a3108b1fa56817ab690045af3c25
Paul Dicker <paul@pitdicker.nl>
2025-04-03 21:26:36 +0200

    Only draw in expose callback in RS01/RS02/RS03 windows

--- src/rs02-verify.c
+++ src/rs02-verify.c
@@ -59,7 +59,8 @@ void ResetRS02VerifyWindow(Method *self)
    wl->lastPercent = 0;
 
    GuiFillSpiral(wl->cmpSpiral, Closure->background);
-   GuiDrawSpiral(wl->cmpSpiral);
+   if (wl->cmpSpiral->widget)
+      gtk_widget_queue_draw(wl->cmpSpiral->widget);
 }
 
 /***
